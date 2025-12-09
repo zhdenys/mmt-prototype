deb29895cd8241dce23b182fd4786511d48d48e2
Petro Shchesniuk <37414778-pshehesnyk4@users.noreply.replit.com>
2025-12-09 16:49:40 +0000

    Add vite config and build script

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: process.env.VITE_BASE || "/",
   plugins: [react()],
   server: {
     host: "0.0.0.0",
@@ -10,6 +11,6 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-  },
-})
+    environment: "jsdom"
+  }
+});
